9c4a1ebb5efe7e3dd56c9dd1022c84f9e232f42d
Sabarimani <sabari@xelpmoc.in>
2018-12-10 16:57:09 +0530

    Add/Update/Delete Users APIs are created

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require("express"),
   app = express(),
-  bodyParser = require("body-parser"),
-  http = require("http");
+  bodyParser = require("body-parser");
 var cors = require("cors");
+
+var usersController = require("./apis/users");
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -10,26 +12,25 @@ app.use(bodyParser.json());
 //The dist folder has our static resources (index.html, css, images)
 app.use(express.static(__dirname + "/dist"));
 
-app.get("/users", function(req, res) {
-  var results = [
-    {
-      results: [
-        {
-          firstName: "Sabarimani",
-          lastName: "Radhakrishnan",
-          email: "rpsabarimani@gmail.com"
-        },
-        {
-          firstName: "Pathma",
-          lastName: "Radhakrishnan",
-          email: "rpathma.r@gmail.com"
-        }
-      ],
-      error: [{ errCode: 0, errMsg: "Data fecthed Successfully" }]
+app.use("/apis/*", function timeLog(req, res, next) {
+  var authentication = req.headers.authorization;
+  if (authentication) {
+    authentication = authentication.replace(/^Basic/, "");
+    authentication = new Buffer(authentication, "base64").toString("utf8");
+    var logInfo = authentication.split(":");
+
+    console.log(logInfo);
+    if (logInfo[0] == "Sabari" && logInfo[1] == "Sabari@123") {
+      next();
+    } else {
+      // res.writeHead(401);
+      res.status(401).send("Invalid Authentication");
     }
-  ];
-  res.json(results);
+  }
+  next();
 });
+
+app.use("/apis/users", usersController);
 
 // redirect all others to the index (HTML5 history)
 app.all("/*", function(req, res) {
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
-const API_URL = "http://127.0.0.1:3000/";
+const API_URL = "http://127.0.0.1:3000/apis/";
 export const USER_API = API_URL + "users";
